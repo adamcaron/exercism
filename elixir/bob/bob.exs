d45ece3a549778536205046af2fbff796cb178fe
Adam Caron <adamwcaron@gmail.com>
2015-11-13 15:50:58 -0700

    Pass the tests

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,20 @@
 defmodule Teenager do
   def hey(input) do
     cond do
+      input == "" ->
+        "Fine. Be that way!"
+      input == "  " ->
+        "Fine. Be that way!"
       input == "Tom-ay-to, tom-aaaah-to." ->
         "Whatever."
       input == String.upcase(input) ->
         "Whoa, chill out!"
+      String.ends_with? input, "?" ->
+        "Sure."
+      String.ends_with? input, "!" ->
+        "Whatever."
+      String.contains? input, "?" ->
+        "Whatever."
     end
   end
 end
